fc7bfca5efdb0758f41591f260f6ce22928d5b9a
daniel <dduenas@tumipos.com>
2021-01-25 10:11:09 -0500

    F

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return response([
-        "message" => "niomads api"
+        "message" => "factura prepago api"
     ]);
 });
 
